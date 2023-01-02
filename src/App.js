eeee744bb49c549729790c5a0dcc5de3f2eebee6
TukadiyaRaju <raju.tukadiya@gmail.com>
2023-01-02 10:23:49 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import { useState, useEffect } from "react";
+import data from "./resources/countryData.json";
+import "./App.css";
+
+function App() {
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    const keyDownHandler = (event) => {
+      if (event.key === "Escape") {
+        setList([]);
+      }
+    };
+
+    document.addEventListener("keydown", keyDownHandler);
+
+    return () => {
+      document.removeEventListener("keydown", keyDownHandler);
+    };
+  }, []);
+
+  function onChangeHandler(el) {
+    let countries = [];
+    data.map((e) => {
+      if (
+        el.target.value &&
+        e.name.toLowerCase().search(el.target.value.toLowerCase()) === 0
+      ) {
+        countries.push(e.name);
+      }
+    });
+    setList(countries);
+  }
+
+  return (
+    <div className="App">
+      <h1>Search</h1>
+      <div className="main">
+        <input onChange={onChangeHandler} type="text" />
+        <button>Search</button>
+        {/* <p>{text}</p> */}
+        <ul className="list">
+          {list.map((e, i) => {
+            return <li key={i}>{e}</li>;
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
